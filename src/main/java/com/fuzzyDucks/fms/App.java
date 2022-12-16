0143a9d9b315bbc29f5c0f841ccbabbdf5f97247
lefdawyy <jehadlefdawy123@gmail.com>
2022-12-16 19:23:01 +0200

    File repository code refactor

--- src/main/java/com/fuzzyDucks/fms/App.java
+++ src/main/java/com/fuzzyDucks/fms/App.java
@@ -1,38 +1,20 @@
 package com.fuzzyDucks.fms;
 
-import com.fuzzyDucks.fms.classification.FileService;
-import com.fuzzyDucks.fms.classification.enums.SortType;
-import com.fuzzyDucks.fms.fileRepository.FileSchema;
-import com.fuzzyDucks.fms.fileRepository.FileServices;
-import org.bson.Document;
+import com.fuzzyDucks.fms.File.FileSchema.FileSchema;
+import com.fuzzyDucks.fms.File.FileService;
 
 import java.io.File;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-
-//        for (Document s :FileService.getFileSortedBy("name",true)) {
-//            System.out.println(s);
-//        }
-//        for (Document s :FileService.getFilesEqaulValue("name","moon")) {
-//            System.out.println(s);
-//        }
-//        for (Document s :FileService.getFilesBySizeGte(7)) {
-//            System.out.println(s);
-//        }
-//        for (Document s :FileService.getFilesBySizeLte(7)) {
-//            System.out.println(s);
-//        }
-        for (Document s :FileService.getSortedBy("name", SortType.DESCENDING)) {
-           System.out.println(s);
-       }
-        File f = new File("D:\\test1\\test.txt");
-        FileSchema file = new FileSchema(f);
-        FileServices.deleteFile("test");
-        FileServices.exportFile("test");
-
-
+    try{
+        //FileSchema file = new FileSchema(new File("D:\\test.doc"));
+        //FileService.exportFile("test","doc");
+        //FileService.deleteFile("test","txt");
+    } catch(Exception e){
+        e.printStackTrace();
+    }
     }
 
 }
