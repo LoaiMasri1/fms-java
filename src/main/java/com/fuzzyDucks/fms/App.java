0f7dd95bbeb986ba19c80810cf5177da17b9b4c1
lefdawyy <jehadlefdawy123@gmail.com>
2022-12-16 03:58:23 +0200

    File repository

--- src/main/java/com/fuzzyDucks/fms/App.java
+++ src/main/java/com/fuzzyDucks/fms/App.java
@@ -2,7 +2,11 @@ package com.fuzzyDucks.fms;
 
 import com.fuzzyDucks.fms.classification.FileService;
 import com.fuzzyDucks.fms.classification.enums.SortType;
+import com.fuzzyDucks.fms.fileRepository.FileSchema;
+import com.fuzzyDucks.fms.fileRepository.FileServices;
 import org.bson.Document;
+
+import java.io.File;
 
 public class App {
 
@@ -23,6 +27,10 @@ public class App {
         for (Document s :FileService.getSortedBy("name", SortType.DESCENDING)) {
            System.out.println(s);
        }
+        File f = new File("D:\\test1\\test.txt");
+        FileSchema file = new FileSchema(f);
+        FileServices.deleteFile("test");
+        FileServices.exportFile("test");
 
 
     }
