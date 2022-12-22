831119a39bdc0415a7f1dfbf1136d82537f5e3d8
aboodjallad <67801795+aboodjallad@users.noreply.github.com>
2022-12-23 01:20:59 +0200

    Update FileSchema.java

--- src/main/java/com/fuzzyDucks/fms/File/FileSchema/FileSchema.java
+++ src/main/java/com/fuzzyDucks/fms/File/FileSchema/FileSchema.java
@@ -1,10 +1,10 @@
 package com.fuzzyDucks.fms.File.FileSchema;
-
 import org.bson.Document;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
-
 import com.fuzzyDucks.fms.File.FileService;
 import com.fuzzyDucks.fms.File.FileUtils;
 import com.fuzzyDucks.fms.File.enums.PathInfo;
@@ -12,6 +12,7 @@ import com.fuzzyDucks.fms.File.enums.PathInfo;
 public class FileSchema {
     private static final int BYTE_TO_BITS = 8;
     private static final int BITS_TO_KILOBITS = 1024;
+    private File file;
     private String name;
     private String path;
     private String type;
@@ -19,17 +20,20 @@ public class FileSchema {
     private Date crtDate;
     private Date updDate;
     private int version;
+    private ArrayList<VersionSchema> versions;
 
     public FileSchema(File file) throws IOException {
+        this.file = file;
         String fileName = file.getName();
         this.name = encodeName(fileName);
         File newFile = new File(newPath(fileName));
         this.path = FileUtils.encodeValue(newFile.getPath());
         this.type = encodeType(fileName);
+        this.versions = new ArrayList<>();
         this.size = (double) (file.length() * BYTE_TO_BITS) / BITS_TO_KILOBITS; // In kilobits
         this.crtDate = new Date();
         this.updDate = new Date();
-        this.version=0;
+        this.version = 0;
         FileService.importFile(this, file, newFile);
     }
 
@@ -39,18 +43,26 @@ public class FileSchema {
         document.append("path", this.path);
         document.append("type", this.type);
         document.append("size", this.size);
+        document.append("versions", this.versions);
+        document.append("version", this.version);
         document.append("crtDate", this.crtDate);
         document.append("updDate", this.updDate);
-        document.append("version", this.version);
-
         return document;
     }
-    
-    public  Document UpdateDocument(int newVersion, String newPath,LocalDateTime newDate) {
+
+    public void defaultFile(File f) throws IOException {
+        String fileName = f.getName();
+        this.name = encodeName(fileName);
+        this.path = FileUtils.encodeValue(f.getPath());
+        FileService.importFile(this, file, f);
+    }
+
+    public Document updateDocument(int newVersion, String newPath) {
         Document document = new Document();
-        document.append("version",newVersion);
-        document.append("updDate",newDate);
-        document.append("path",newPath);
+        document.append("version", newVersion);
+        document.append("updDate", LocalDate.now());
+        document.append("path", FileUtils.encodeValue(newPath));
+
 
         return document;
     }
@@ -81,4 +93,32 @@ public class FileSchema {
     public String getType() {
         return type;
     }
+
+    public String getPath() {
+        return path;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public Date getCrtDate() {
+        return crtDate;
+    }
+
+    public Date getUpdDate() {
+        return updDate;
+    }
+
+    public int getVersion() {
+        return version;
+    }
 }
+
+
+
+
