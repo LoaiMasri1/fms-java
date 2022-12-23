52cd742d3484f0100a8dc4db292ea9debcea10fe
muthana-abo-alez1 <muthana.192837@gmail.com>
2022-12-23 17:55:08 +0200

    LoggingHandler

---
+++ src/main/java/com/fuzzyDucks/fms/Logger/LoggingHandler.java
@@ -0,0 +1,44 @@
+package com.fuzzyDucks.fms.Logger;
+
+
+import java.util.logging.*;
+
+public class LoggingHandler implements ILogger {
+    private static ILogger instance = null;
+    private static Logger logger = Logger.getLogger(LoggingHandler.class.getSimpleName());
+
+    private LoggingHandler() {
+        try {
+            logger.setLevel(Level.ALL);
+            logger.setUseParentHandlers(false);
+            FileHandler fh = new FileHandler("log.log", true);
+            fh.setFormatter(new LoggerFormat());
+            logger.addHandler(fh);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ILogger getInstance() {
+        if (instance == null) {
+            instance = new LoggingHandler();
+        }
+        return instance;
+    }
+
+    @Override
+    public void logWarning(String message) {
+        logger.warning(message);
+    }
+
+    @Override
+    public void logInfo(String message) {
+        logger.info(message);
+
+    }
+
+    @Override
+    public void logDebug(String message) {
+        logger.fine(message);
+    }
+}
